1704f17de06f6249810325b670913244dc7d51e1
MaverickSevmont (Erick Sevilla) <sevmont@gmail.com>
2021-12-12 13:25:33 -0600

    Create Scan-Log4j.ps1

---
+++ Scan-Log4j.ps1
@@ -0,0 +1,17 @@
+    # Find Log4j library instances in all Windows OS Drives and Directories
+    # Requires Invoke-Parallel.ps1 https://github.com/mavericksevmont/Invoke-Parallel
+$Cred = Get-Credential
+$Hosts = Get-Content $PSScriptRoot\Hosts.txt
+Import-Module $PSScriptRoot\Invoke-Parallel.ps1 # Get this from fork and add it in same directory: https://github.com/mavericksevmont/Invoke-Parallel
+
+$Hosts | Invoke-Parallel -ScriptBlock {
+
+Invoke-Command -ComputerName $_ -ScriptBlock {
+get-psdrive -PSProvider "FileSystem" | 
+% {$Drive = $_.Root ; Get-ChildItem $Drive -Include *log4j*.jar* -Recurse -ErrorAction SilentlyContinue} | 
+select @{n='Server';e={$env:COMPUTERNAME}},BaseName,Directory,CreationTimeUtc,LastAccessTimeUtc,LastWriteTimeUtc
+} -Credential $Cred
+
+} -OutVariable Results -ImportVariables
+
+$Results | Export-csv $PSScriptRoot\Resultslog4_HTM.csv -Force -NoTypeInformation
