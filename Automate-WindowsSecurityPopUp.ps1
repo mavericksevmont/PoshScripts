802941ff8124c4e1e6697b01a5afa9065d32136a
MaverickSevmont (Erick Sevilla) <sevmont@gmail.com>
2022-03-09 10:55:38 -0600

    Create Automate-WindowsSecurityPopUp.ps1

---
+++ Automate-WindowsSecurityPopUp.ps1
@@ -0,0 +1,37 @@
+# PorwerShell - Automate Windows Security Pop-up
+# List Current Windows: Get-Process | Where-Object {$_.MainWindowTitle -ne ""} | Select-Object MainWindowTitle
+# Thanks to https://johnlouros.com/blog/how-to-automate-windows-security-prompt-input, this is based on his FindWindows function.
+
+$Window  = 'Windows Security'
+$PIN     = '0123456789' # Use Credential Manager or Secret Vault instead to grab this
+$Retries = '60' 
+
+Function Find-Window {
+[CmdletBinding()]Param (
+[string]$WindowName,
+[int]$Retries = 10,
+[int]$SleepInterval = 1000
+)
+
+[int]$CurrentTry = 0;
+[bool]$WindowsFound = $false;
+
+Add-Type -AssemblyName Microsoft.VisualBasic
+Add-Type -AssemblyName System.Windows.Forms
+
+Do {
+     $CurrentTry++;
+     Start-Sleep -Milliseconds $SleepInterval
+     Try   { [Microsoft.VisualBasic.Interaction]::AppActivate($windowName) ; $windowFound = $true; }
+     Catch { Write-Information "[$currentTry out of $Retries] Searching for Window with title '$WindowName'" ; $WindowFound = $false }
+   } While ($CurrentTry -lt $retries -and $WindowFound -eq $false)
+     return $WindowFound
+}
+
+if ( Find-Window -WindowName $Window -Retries $Retries ) {
+        Start-Sleep -Milliseconds 250
+        [System.Windows.Forms.SendKeys]::SendWait($PIN)
+        [System.Windows.Forms.SendKeys]::SendWait('{TAB}')
+        [System.Windows.Forms.SendKeys]::SendWait('{TAB}')
+        [System.Windows.Forms.SendKeys]::SendWait('{ENTER}')
+} else {break}
