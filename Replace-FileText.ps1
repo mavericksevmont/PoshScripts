d780cd4fe825fd00454af8929d0f9f22435b2864
MaverickSevmont (Erick Sevilla) <sevmont@gmail.com>
2022-05-25 14:32:22 -0500

    Update Replace-FileText.ps1

--- Replace-FileText.ps1
+++ Replace-FileText.ps1
@@ -2,10 +2,10 @@ function Replace-FileText {
 [CmdletBinding()]
 [Alias("rft")]
 Param($FileName,$SearchFor,$ReplaceWith)
-$Files = Get-ChildItem $FileName -Recurse -ErrorAction Stop
-$Files | % {(Get-Content $_) -Replace "$SearchFor","$ReplaceWith" | Set-Content $_.FullName }
-}
-
+    $Files = Get-ChildItem $FileName -Recurse -ErrorAction Stop
+    $Files | ForEach-Object {(Get-Content $_) -Replace "$SearchFor","$ReplaceWith" | Set-Content $_.FullName }
+    }
+    
 # Examples:
 # rft -FileName "C:\Tests\FileName*.txt" -SearchFor 'OldWord' -ReplaceWith 'NewWord'
 # Replace-FileText -FileName "C:\Tests\FileName*.txt" -SearchFor 'OldWord' -ReplaceWith 'NewWord'
