5d2360c4615a16e79176cacf24cf497de5aadef8
E3k <sevmont@gmail.com>
2022-08-10 21:02:10 -0500

    Update Get-AnsibleTowerTaskResult.ps1

--- Get-AnsibleTowerTaskResult.ps1
+++ Get-AnsibleTowerTaskResult.ps1
@@ -6,6 +6,6 @@ $headers.Add("ask_credential_on_launch", "false")
 $JobID    = '337947' # The Job ID
 # $ TaskName is the Name of the task you want to pull the data from, replace spaces with %20, output from this task must be json format, e.g. "| to_json"
 $TaskName = 'Print%20Script%20Results' 
-$URI      = "https://glgtsansprd.stn.nl.ao-srv.com/api/v2/jobs/$JobId/job_events/?event__contains=runner_on_ok&task__contains=$TaskName"
+$URI      = "https://ansibletowerdomainurlthingnamegoeshere/api/v2/jobs/$JobId/job_events/?event__contains=runner_on_ok&task__contains=$TaskName"
 $Results = Invoke-RestMethod -Uri $URI -Headers $headers
 $Results.results.event_data.res.msg # try to surf the previous properties if you don't see your data
